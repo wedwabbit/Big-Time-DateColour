de1280486cb0e5ba7058ff34888bd22acbdbb78d
Kevin Grant <kevinwgrant@gmail.com>
2016-01-06 10:55:07 +1000

    Added scripts to create digital fonts.

---
+++ digitalthick.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# 1
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 61,12,69,37' -draw 'rectangle 62,11,68,11' -draw 'rectangle 64,10,66,10' -draw 'rectangle 62,38,68,38' -draw 'rectangle 64,39,66,39' \
+-draw 'rectangle 61,47,69,72' -draw 'rectangle 62,46,68,46' -draw 'rectangle 64,45,66,45' -draw 'rectangle 62,73,68,73' -draw 'rectangle 64,74,66,74' \
+font_1.png
+
+#2
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 14,3,58,11' -draw 'rectangle 13,4,13,10' -draw 'rectangle 12,6,12,8' -draw 'rectangle 59,4,59,10' -draw 'rectangle 60,6,60,8' \
+-draw 'rectangle 61,12,69,37' -draw 'rectangle 62,11,68,11' -draw 'rectangle 64,10,66,10' -draw 'rectangle 62,38,68,38' -draw 'rectangle 64,39,66,39' \
+-draw 'rectangle 14,38,58,46' -draw 'rectangle 13,39,13,45' -draw 'rectangle 12,41,12,43' -draw 'rectangle 59,39,59,45' -draw 'rectangle 60,41,60,43' \
+-draw 'rectangle 3,47,11,72' -draw 'rectangle 4,46,10,46' -draw 'rectangle 6,45,8,45' -draw 'rectangle 4,73,10,73' -draw 'rectangle 6,74,8,74' \
+-draw 'rectangle 14,73,58,81' -draw 'rectangle 13,74,13,80' -draw 'rectangle 12,76,12,78' -draw 'rectangle 59,74,59,80' -draw 'rectangle 60,76,60,78' \
+font_2.png
+
+#3
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 14,3,58,11' -draw 'rectangle 13,4,13,10' -draw 'rectangle 12,6,12,8' -draw 'rectangle 59,4,59,10' -draw 'rectangle 60,6,60,8' \
+-draw 'rectangle 61,12,69,37' -draw 'rectangle 62,11,68,11' -draw 'rectangle 64,10,66,10' -draw 'rectangle 62,38,68,38' -draw 'rectangle 64,39,66,39' \
+-draw 'rectangle 14,38,58,46' -draw 'rectangle 13,39,13,45' -draw 'rectangle 12,41,12,43' -draw 'rectangle 59,39,59,45' -draw 'rectangle 60,41,60,43' \
+-draw 'rectangle 61,47,69,72' -draw 'rectangle 62,46,68,46' -draw 'rectangle 64,45,66,45' -draw 'rectangle 62,73,68,73' -draw 'rectangle 64,74,66,74' \
+-draw 'rectangle 14,73,58,81' -draw 'rectangle 13,74,13,80' -draw 'rectangle 12,76,12,78' -draw 'rectangle 59,74,59,80' -draw 'rectangle 60,76,60,78' \
+font_3.png
+
+#4
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 3,12,11,37' -draw 'rectangle 4,11,10,11' -draw 'rectangle 6,10,8,10' -draw 'rectangle 4,38,10,38' -draw 'rectangle 6,39,8,39' \
+-draw 'rectangle 61,12,69,37' -draw 'rectangle 62,11,68,11' -draw 'rectangle 64,10,66,10' -draw 'rectangle 62,38,68,38' -draw 'rectangle 64,39,66,39' \
+-draw 'rectangle 14,38,58,46' -draw 'rectangle 13,39,13,45' -draw 'rectangle 12,41,12,43' -draw 'rectangle 59,39,59,45' -draw 'rectangle 60,41,60,43' \
+-draw 'rectangle 61,47,69,72' -draw 'rectangle 62,46,68,46' -draw 'rectangle 64,45,66,45' -draw 'rectangle 62,73,68,73' -draw 'rectangle 64,74,66,74' \
+font_4.png
+
+#5
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 14,3,58,11' -draw 'rectangle 13,4,13,10' -draw 'rectangle 12,6,12,8' -draw 'rectangle 59,4,59,10' -draw 'rectangle 60,6,60,8' \
+-draw 'rectangle 3,12,11,37' -draw 'rectangle 4,11,10,11' -draw 'rectangle 6,10,8,10' -draw 'rectangle 4,38,10,38' -draw 'rectangle 6,39,8,39' \
+-draw 'rectangle 14,38,58,46' -draw 'rectangle 13,39,13,45' -draw 'rectangle 12,41,12,43' -draw 'rectangle 59,39,59,45' -draw 'rectangle 60,41,60,43' \
+-draw 'rectangle 61,47,69,72' -draw 'rectangle 62,46,68,46' -draw 'rectangle 64,45,66,45' -draw 'rectangle 62,73,68,73' -draw 'rectangle 64,74,66,74' \
+-draw 'rectangle 14,73,58,81' -draw 'rectangle 13,74,13,80' -draw 'rectangle 12,76,12,78' -draw 'rectangle 59,74,59,80' -draw 'rectangle 60,76,60,78' \
+font_5.png
+
+#6
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 14,3,58,11' -draw 'rectangle 13,4,13,10' -draw 'rectangle 12,6,12,8' -draw 'rectangle 59,4,59,10' -draw 'rectangle 60,6,60,8' \
+-draw 'rectangle 3,12,11,37' -draw 'rectangle 4,11,10,11' -draw 'rectangle 6,10,8,10' -draw 'rectangle 4,38,10,38' -draw 'rectangle 6,39,8,39' \
+-draw 'rectangle 14,38,58,46' -draw 'rectangle 13,39,13,45' -draw 'rectangle 12,41,12,43' -draw 'rectangle 59,39,59,45' -draw 'rectangle 60,41,60,43' \
+-draw 'rectangle 3,47,11,72' -draw 'rectangle 4,46,10,46' -draw 'rectangle 6,45,8,45' -draw 'rectangle 4,73,10,73' -draw 'rectangle 6,74,8,74' \
+-draw 'rectangle 61,47,69,72' -draw 'rectangle 62,46,68,46' -draw 'rectangle 64,45,66,45' -draw 'rectangle 62,73,68,73' -draw 'rectangle 64,74,66,74' \
+-draw 'rectangle 14,73,58,81' -draw 'rectangle 13,74,13,80' -draw 'rectangle 12,76,12,78' -draw 'rectangle 59,74,59,80' -draw 'rectangle 60,76,60,78' \
+font_6.png
+
+#7
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 14,3,58,11' -draw 'rectangle 13,4,13,10' -draw 'rectangle 12,6,12,8' -draw 'rectangle 59,4,59,10' -draw 'rectangle 60,6,60,8' \
+-draw 'rectangle 61,12,69,37' -draw 'rectangle 62,11,68,11' -draw 'rectangle 64,10,66,10' -draw 'rectangle 62,38,68,38' -draw 'rectangle 64,39,66,39' \
+-draw 'rectangle 61,47,69,72' -draw 'rectangle 62,46,68,46' -draw 'rectangle 64,45,66,45' -draw 'rectangle 62,73,68,73' -draw 'rectangle 64,74,66,74' \
+font_7.png
+
+#8
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 14,3,58,11' -draw 'rectangle 13,4,13,10' -draw 'rectangle 12,6,12,8' -draw 'rectangle 59,4,59,10' -draw 'rectangle 60,6,60,8' \
+-draw 'rectangle 3,12,11,37' -draw 'rectangle 4,11,10,11' -draw 'rectangle 6,10,8,10' -draw 'rectangle 4,38,10,38' -draw 'rectangle 6,39,8,39' \
+-draw 'rectangle 61,12,69,37' -draw 'rectangle 62,11,68,11' -draw 'rectangle 64,10,66,10' -draw 'rectangle 62,38,68,38' -draw 'rectangle 64,39,66,39' \
+-draw 'rectangle 14,38,58,46' -draw 'rectangle 13,39,13,45' -draw 'rectangle 12,41,12,43' -draw 'rectangle 59,39,59,45' -draw 'rectangle 60,41,60,43' \
+-draw 'rectangle 3,47,11,72' -draw 'rectangle 4,46,10,46' -draw 'rectangle 6,45,8,45' -draw 'rectangle 4,73,10,73' -draw 'rectangle 6,74,8,74' \
+-draw 'rectangle 61,47,69,72' -draw 'rectangle 62,46,68,46' -draw 'rectangle 64,45,66,45' -draw 'rectangle 62,73,68,73' -draw 'rectangle 64,74,66,74' \
+-draw 'rectangle 14,73,58,81' -draw 'rectangle 13,74,13,80' -draw 'rectangle 12,76,12,78' -draw 'rectangle 59,74,59,80' -draw 'rectangle 60,76,60,78' \
+font_8.png
+
+#9
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 14,3,58,11' -draw 'rectangle 13,4,13,10' -draw 'rectangle 12,6,12,8' -draw 'rectangle 59,4,59,10' -draw 'rectangle 60,6,60,8' \
+-draw 'rectangle 3,12,11,37' -draw 'rectangle 4,11,10,11' -draw 'rectangle 6,10,8,10' -draw 'rectangle 4,38,10,38' -draw 'rectangle 6,39,8,39' \
+-draw 'rectangle 61,12,69,37' -draw 'rectangle 62,11,68,11' -draw 'rectangle 64,10,66,10' -draw 'rectangle 62,38,68,38' -draw 'rectangle 64,39,66,39' \
+-draw 'rectangle 14,38,58,46' -draw 'rectangle 13,39,13,45' -draw 'rectangle 12,41,12,43' -draw 'rectangle 59,39,59,45' -draw 'rectangle 60,41,60,43' \
+-draw 'rectangle 61,47,69,72' -draw 'rectangle 62,46,68,46' -draw 'rectangle 64,45,66,45' -draw 'rectangle 62,73,68,73' -draw 'rectangle 64,74,66,74' \
+-draw 'rectangle 14,73,58,81' -draw 'rectangle 13,74,13,80' -draw 'rectangle 12,76,12,78' -draw 'rectangle 59,74,59,80' -draw 'rectangle 60,76,60,78' \
+font_9.png
+
+#0
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 14,3,58,11' -draw 'rectangle 13,4,13,10' -draw 'rectangle 12,6,12,8' -draw 'rectangle 59,4,59,10' -draw 'rectangle 60,6,60,8' \
+-draw 'rectangle 3,12,11,37' -draw 'rectangle 4,11,10,11' -draw 'rectangle 6,10,8,10' -draw 'rectangle 4,38,10,38' -draw 'rectangle 6,39,8,39' \
+-draw 'rectangle 61,12,69,37' -draw 'rectangle 62,11,68,11' -draw 'rectangle 64,10,66,10' -draw 'rectangle 62,38,68,38' -draw 'rectangle 64,39,66,39' \
+-draw 'rectangle 3,47,11,72' -draw 'rectangle 4,46,10,46' -draw 'rectangle 6,45,8,45' -draw 'rectangle 4,73,10,73' -draw 'rectangle 6,74,8,74' \
+-draw 'rectangle 61,47,69,72' -draw 'rectangle 62,46,68,46' -draw 'rectangle 64,45,66,45' -draw 'rectangle 62,73,68,73' -draw 'rectangle 64,74,66,74' \
+-draw 'rectangle 14,73,58,81' -draw 'rectangle 13,74,13,80' -draw 'rectangle 12,76,12,78' -draw 'rectangle 59,74,59,80' -draw 'rectangle 60,76,60,78' \
+font_0.png
+
+#Negate
+for file in `ls font_*.png` ; do
+    convert $file -negate new.png
+    mv new.png $file
+done
+
---
+++ digitalthin.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# 1
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 65,9,69,38' -draw 'rectangle 66,8,68,8' -draw 'point 67,7' -draw 'rectangle 66,39,68,39' -draw 'point 67,40' \
+-draw 'rectangle 65,46,69,75' -draw 'rectangle 66,45,68,45' -draw 'point 67,44' -draw 'rectangle 66,76,68,76' -draw 'point 67,77' \
+font_1.png
+
+#2
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 9,3,63,7' -draw 'rectangle 8,4,8,6' -draw 'point 7,5' -draw 'rectangle 64,4,64,6' -draw 'point 65,5' \
+-draw 'rectangle 65,9,69,38' -draw 'rectangle 66,8,68,8' -draw 'point 67,7' -draw 'rectangle 66,39,68,39' -draw 'point 67,40' \
+-draw 'rectangle 9,40,63,44' -draw 'rectangle 8,41,8,43' -draw 'point 7,42' -draw 'rectangle 64,41,64,43' -draw 'point 65,42' \
+-draw 'rectangle 3,46,7,75' -draw 'rectangle 4,45,6,45' -draw 'point 5,44' -draw 'rectangle 4,76,6,76' -draw 'point 5,77' \
+-draw 'rectangle 9,77,63,81' -draw 'rectangle 8,78,8,80' -draw 'point 7,79' -draw 'rectangle 64,78,64,80' -draw 'point 65,79' \
+font_2.png
+
+#3
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 9,3,63,7' -draw 'rectangle 8,4,8,6' -draw 'point 7,5' -draw 'rectangle 64,4,64,6' -draw 'point 65,5' \
+-draw 'rectangle 65,9,69,38' -draw 'rectangle 66,8,68,8' -draw 'point 67,7' -draw 'rectangle 66,39,68,39' -draw 'point 67,40' \
+-draw 'rectangle 9,40,63,44' -draw 'rectangle 8,41,8,43' -draw 'point 7,42' -draw 'rectangle 64,41,64,43' -draw 'point 65,42' \
+-draw 'rectangle 65,46,69,75' -draw 'rectangle 66,45,68,45' -draw 'point 67,44' -draw 'rectangle 66,76,68,76' -draw 'point 67,77' \
+-draw 'rectangle 9,77,63,81' -draw 'rectangle 8,78,8,80' -draw 'point 7,79' -draw 'rectangle 64,78,64,80' -draw 'point 65,79' \
+font_3.png
+
+#4
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 3,9,7,38' -draw 'rectangle 4,8,6,8' -draw 'point 5,7' -draw 'rectangle 4,39,6,39' -draw 'point 5,40' \
+-draw 'rectangle 65,9,69,38' -draw 'rectangle 66,8,68,8' -draw 'point 67,7' -draw 'rectangle 66,39,68,39' -draw 'point 67,40' \
+-draw 'rectangle 9,40,63,44' -draw 'rectangle 8,41,8,43' -draw 'point 7,42' -draw 'rectangle 64,41,64,43' -draw 'point 65,42' \
+-draw 'rectangle 65,46,69,75' -draw 'rectangle 66,45,68,45' -draw 'point 67,44' -draw 'rectangle 66,76,68,76' -draw 'point 67,77' \
+font_4.png
+
+#5
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 9,3,63,7' -draw 'rectangle 8,4,8,6' -draw 'point 7,5' -draw 'rectangle 64,4,64,6' -draw 'point 65,5' \
+-draw 'rectangle 3,9,7,38' -draw 'rectangle 4,8,6,8' -draw 'point 5,7' -draw 'rectangle 4,39,6,39' -draw 'point 5,40' \
+-draw 'rectangle 9,40,63,44' -draw 'rectangle 8,41,8,43' -draw 'point 7,42' -draw 'rectangle 64,41,64,43' -draw 'point 65,42' \
+-draw 'rectangle 65,46,69,75' -draw 'rectangle 66,45,68,45' -draw 'point 67,44' -draw 'rectangle 66,76,68,76' -draw 'point 67,77' \
+-draw 'rectangle 9,77,63,81' -draw 'rectangle 8,78,8,80' -draw 'point 7,79' -draw 'rectangle 64,78,64,80' -draw 'point 65,79' \
+font_5.png
+
+#6
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 9,3,63,7' -draw 'rectangle 8,4,8,6' -draw 'point 7,5' -draw 'rectangle 64,4,64,6' -draw 'point 65,5' \
+-draw 'rectangle 3,9,7,38' -draw 'rectangle 4,8,6,8' -draw 'point 5,7' -draw 'rectangle 4,39,6,39' -draw 'point 5,40' \
+-draw 'rectangle 9,40,63,44' -draw 'rectangle 8,41,8,43' -draw 'point 7,42' -draw 'rectangle 64,41,64,43' -draw 'point 65,42' \
+-draw 'rectangle 3,46,7,75' -draw 'rectangle 4,45,6,45' -draw 'point 5,44' -draw 'rectangle 4,76,6,76' -draw 'point 5,77' \
+-draw 'rectangle 65,46,69,75' -draw 'rectangle 66,45,68,45' -draw 'point 67,44' -draw 'rectangle 66,76,68,76' -draw 'point 67,77' \
+-draw 'rectangle 9,77,63,81' -draw 'rectangle 8,78,8,80' -draw 'point 7,79' -draw 'rectangle 64,78,64,80' -draw 'point 65,79' \
+font_6.png
+
+#7
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 9,3,63,7' -draw 'rectangle 8,4,8,6' -draw 'point 7,5' -draw 'rectangle 64,4,64,6' -draw 'point 65,5' \
+-draw 'rectangle 65,9,69,38' -draw 'rectangle 66,8,68,8' -draw 'point 67,7' -draw 'rectangle 66,39,68,39' -draw 'point 67,40' \
+-draw 'rectangle 65,46,69,75' -draw 'rectangle 66,45,68,45' -draw 'point 67,44' -draw 'rectangle 66,76,68,76' -draw 'point 67,77' \
+font_7.png
+
+#8
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 9,3,63,7' -draw 'rectangle 8,4,8,6' -draw 'point 7,5' -draw 'rectangle 64,4,64,6' -draw 'point 65,5' \
+-draw 'rectangle 3,9,7,38' -draw 'rectangle 4,8,6,8' -draw 'point 5,7' -draw 'rectangle 4,39,6,39' -draw 'point 5,40' \
+-draw 'rectangle 65,9,69,38' -draw 'rectangle 66,8,68,8' -draw 'point 67,7' -draw 'rectangle 66,39,68,39' -draw 'point 67,40' \
+-draw 'rectangle 9,40,63,44' -draw 'rectangle 8,41,8,43' -draw 'point 7,42' -draw 'rectangle 64,41,64,43' -draw 'point 65,42' \
+-draw 'rectangle 3,46,7,75' -draw 'rectangle 4,45,6,45' -draw 'point 5,44' -draw 'rectangle 4,76,6,76' -draw 'point 5,77' \
+-draw 'rectangle 65,46,69,75' -draw 'rectangle 66,45,68,45' -draw 'point 67,44' -draw 'rectangle 66,76,68,76' -draw 'point 67,77' \
+-draw 'rectangle 9,77,63,81' -draw 'rectangle 8,78,8,80' -draw 'point 7,79' -draw 'rectangle 64,78,64,80' -draw 'point 65,79' \
+font_8.png
+
+#9
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 9,3,63,7' -draw 'rectangle 8,4,8,6' -draw 'point 7,5' -draw 'rectangle 64,4,64,6' -draw 'point 65,5' \
+-draw 'rectangle 3,9,7,38' -draw 'rectangle 4,8,6,8' -draw 'point 5,7' -draw 'rectangle 4,39,6,39' -draw 'point 5,40' \
+-draw 'rectangle 65,9,69,38' -draw 'rectangle 66,8,68,8' -draw 'point 67,7' -draw 'rectangle 66,39,68,39' -draw 'point 67,40' \
+-draw 'rectangle 9,40,63,44' -draw 'rectangle 8,41,8,43' -draw 'point 7,42' -draw 'rectangle 64,41,64,43' -draw 'point 65,42' \
+-draw 'rectangle 65,46,69,75' -draw 'rectangle 66,45,68,45' -draw 'point 67,44' -draw 'rectangle 66,76,68,76' -draw 'point 67,77' \
+-draw 'rectangle 9,77,63,81' -draw 'rectangle 8,78,8,80' -draw 'point 7,79' -draw 'rectangle 64,78,64,80' -draw 'point 65,79' \
+font_9.png
+
+#0
+convert -size 72x84 canvas:white -stroke black \
+-draw 'rectangle 9,3,63,7' -draw 'rectangle 8,4,8,6' -draw 'point 7,5' -draw 'rectangle 64,4,64,6' -draw 'point 65,5' \
+-draw 'rectangle 3,9,7,38' -draw 'rectangle 4,8,6,8' -draw 'point 5,7' -draw 'rectangle 4,39,6,39' -draw 'point 5,40' \
+-draw 'rectangle 65,9,69,38' -draw 'rectangle 66,8,68,8' -draw 'point 67,7' -draw 'rectangle 66,39,68,39' -draw 'point 67,40' \
+-draw 'rectangle 3,46,7,75' -draw 'rectangle 4,45,6,45' -draw 'point 5,44' -draw 'rectangle 4,76,6,76' -draw 'point 5,77' \
+-draw 'rectangle 65,46,69,75' -draw 'rectangle 66,45,68,45' -draw 'point 67,44' -draw 'rectangle 66,76,68,76' -draw 'point 67,77' \
+-draw 'rectangle 9,77,63,81' -draw 'rectangle 8,78,8,80' -draw 'point 7,79' -draw 'rectangle 64,78,64,80' -draw 'point 65,79' \
+font_0.png
+
+#Negate
+for file in `ls font_*.png` ; do
+    convert $file -negate new.png
+    mv new.png $file
+done
+
+
